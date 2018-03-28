563aa9eb3cdb0e0fb0c139f9def1fac467405323
Tengku Huday Ilahi <tengkuhuday@gmail.com>
2018-03-28 17:22:44 +0700

    Huahahaha

--- flat/settings.py
+++ flat/settings.py
@@ -106,7 +106,7 @@ ALLOWED_HOSTS = ["localhost"]
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
